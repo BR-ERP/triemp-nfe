e73a0b4a2de33339c4812a33dcb39c7e7c4f9934
paulo-triemp <paulo-triemp@11de3349-2665-4705-95c0-e7a727960e46>
2011-11-09 16:52:12 +0000

    Alterações para atender as novas regras, e novo schema

--- trunk/triemp-nfe200/src/br/com/triemp/nfe/util/NFeUtil.java
+++ trunk/triemp-nfe200/src/br/com/triemp/nfe/util/NFeUtil.java
@@ -108,30 +108,25 @@ public class NFeUtil {
 		return valueToPad;
 	}
 	
-	public static boolean isValidBarCode(String barCode) {
-		int len = barCode.length();
-		if(len == 8 || len == 12 || len == 13 || len == 14){
-			return true;
-		}
-		return false;
-		/*
-		int digit;
-		int calculated;
-		String ean;
-		String checkSum = "131313131313";
-		int sum = 0;
-		if (barCode.length() == 8 || barCode.length() == 13) {
-			digit = Integer.parseInt("" + barCode.charAt(barCode.length() - 1));
-			ean = barCode.substring(0, barCode.length() - 1);
-			for (int i = 0; i <= ean.length() - 1; i++) {
-			sum += (Integer.parseInt("" + ean.charAt(i))) * (Integer.parseInt("" + checkSum.charAt(i)));
-			}
-			calculated = 10 - (sum % 10);
-			return (digit == calculated);
-		} else {
-			return false;
-		}*/
-	}
+	public static boolean isValidBarCodeEAN(String barCode) {
+        int digit;
+        int calculated;
+        String ean;
+        String checkSum = "131313131313";
+        int sum = 0;
+
+        if (barCode.length() == 8 || barCode.length() == 13) {
+            digit = Integer.parseInt("" + barCode.charAt(barCode.length() - 1)); 
+            ean = barCode.substring(0, barCode.length() - 1);            
+            for (int i = 0; i <= ean.length() - 1; i++) {
+                sum += (Integer.parseInt("" + ean.charAt(i))) * (Integer.parseInt("" + checkSum.charAt(i)));
+            }            
+            calculated = 10 - (sum % 10);
+            return (digit == calculated);
+        } else {
+            return false;
+        }
+    }
 	
 	public static String tratarNcm(String ncm){
 		if(ncm.length() > 0){
